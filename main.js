4268df7dc7e9d5566c2a13eb08e76a91be4d5e95
DeepakSharma234 <132416756+DeepakSharma234@users.noreply.github.com>
2023-05-04 19:16:37 +0530

    main.js

--- main.js
+++ main.js
@@ -3,15 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
-
-
-// USING API FETCH REQUEST FOR QUOTES
+const loader = document.getElementById('loader');
 
 let apiQuotes = []
+
+//SHOW LOADING
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//HIDE LOADING
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true; 
+}
 
 // Show new quotes
 
 function newQuotes(){
+    loading();
     // Pick a random quote from apiQuotes
     const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
     
@@ -29,13 +40,17 @@ function newQuotes(){
     } else {
         quoteText.classList.remove('long-quote');
     }
+
+    //SET QUOTE HIDE LOADER
     quoteText.textContent = quote.text;
+    complete();
 }
 
 
 // Get Quotes from API
 
 async function getQuotes(){
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try{
         const response = await fetch(apiUrl);
@@ -64,8 +79,7 @@ twitterBtn.addEventListener('click', tweetQuote);
 getQuotes()
 
 
-
-
+// --------------------------------------------------------------------------------------
 // USING LOCAL QUOTES AVAILABLE LOCALLY IN ANOTHER FILE
 // function newQuote(){
 //     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
@@ -75,3 +89,4 @@ getQuotes()
 
 
 // newQuote()
+// -----------------------------------------------------------------------------------------
